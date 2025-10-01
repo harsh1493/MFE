22db1f4c12467a65f4264f01f45d74bc94e900b5
harsh1493 <73359808+harsh1493@users.noreply.github.com>
2025-10-01 17:04:05 +0530

    Initial Commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+// export default ()=>{
+//     return <h1>Helllo from Container</h1>;
+// }
+
+export default ()=>{
+ 
+    return <>
+    <h1>Helllo from Container</h1>
+       <MarketingApp/>; 
+    </>;
+}
