81fec5935a2af30d5312cc1b66f8a6838216fdc4
harsh1493 <73359808+harsh1493@users.noreply.github.com>
2025-10-05 13:36:10 +0530

    added marketing deployment

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -14,7 +14,7 @@ const prodConfig={
             name: 'marketing',
             filename:'remoteEntry.js',
             exposes:{
-                'MarketingApp':'./src/bootstrap'
+                'marketingApp':'./src/bootstrap'
             },
             shared: packageJson.dependencies
         })
