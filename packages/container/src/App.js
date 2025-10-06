25afb6cd9863b84cb6ff5010f872a733004e25c8
harsh1493 <73359808+harsh1493@users.noreply.github.com>
2025-10-07 01:53:46 +0530

    added header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import MarketingApp from "./components/marketingApp";
+import MarketingApp from "./components/MarketingApp";
 import Header from './components/Header';
 // export default ()=>{
 //     return <h1>Helllo from Container</h1>;
